3565f5b3fbab2578125bfddae9893f357fbd0e22
anshumankumarnidhi <137386279+Anshuman122@users.noreply.github.com>
2025-09-30 11:15:11 +0530

    config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     
     APP_NAME: str = "SAI Talent Assessment"
-    SECRET_KEY: str = "your_super_secret_key"
+    SECRET_KEY: str = "super_secret_key"
     
     
     DATABASE_URL: str = "sqlite:///./sai_talent.db"
