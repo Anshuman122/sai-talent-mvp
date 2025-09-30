7daf60d265743eb7e75d5995753df9ed5ff5bfbf
anshumankumarnidhi <137386279+Anshuman122@users.noreply.github.com>
2025-09-30 11:09:28 +0530

    shuttle_run.py

--- ai-modules/tests/shuttle_run.py
+++ ai-modules/tests/shuttle_run.py
@@ -87,8 +87,8 @@ if __name__ == '__main__':
     
     try:
         result = analyze_shuttle_run(args.video_path)
-        print("✅ Analysis Successful!")
+        print("Analysis Successful!")
         print(result.model_dump_json(indent=4))
     except Exception as e:
-        print(f"🔴 Analysis Failed: {e}")
+        print(f"Analysis Failed: {e}")
 
