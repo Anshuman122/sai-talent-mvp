bdfb2d3b5074f1ebe2d42e4e458495453e835707
anshumankumarnidhi <137386279+Anshuman122@users.noreply.github.com>
2025-09-30 11:15:38 +0530

    database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -15,10 +15,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    """
-    Dependency to get a database session for each request.
-    Ensures the session is always closed after the request.
-    """
+
     db = SessionLocal()
     try:
         yield db
